458691b255de59937965214b8f26ddc2f9c8b72b
GermanoC <105460981+GermanoCesarSS@users.noreply.github.com>
2025-09-25 19:49:44 -0300

    Removendo pasta constansts

--- lib/src/client/meal_db_adapter.dart
+++ lib/src/client/meal_db_adapter.dart
@@ -1,11 +1,10 @@
 import 'package:meal_client_v2/meal_client_v2.dart';
-import 'package:meal_client_v2/src/constants/meal/const_meal.dart';
 import 'package:meal_client_v2/src/database/services/hive_service.dart';
 
 enum MealDataBaseError { notFound, outdated }
 
 class MealClientDBAdapter{
-  final HiveService dataBase = HiveService(boxName: boxNameMeal);
+  final HiveService dataBase = HiveService(boxName: DatabaseBoxes.cache.name);
 
   void save(key, value) async {
     await dataBase.write(
@@ -32,4 +31,5 @@ class MealClientDBAdapter{
   void delete(key) {
     // TODO: implement delete
   }
+
 }
